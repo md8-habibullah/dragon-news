44074021414b17de0c9dfb7df7beedb337509914
md8-habibullah <md8.habibullah@gmail.com>
2024-12-21 12:38:31 +0600

    fix

--- src/components/FindUS.jsx
+++ src/components/FindUS.jsx
@@ -3,7 +3,7 @@ import { FaFacebook, FaInstagram, FaTwitter } from "react-icons/fa";
 export default function FindUS() {
   return (
     <div className="*:w-full bg-base-100 space-y-2">
-      <div className="join join-vertical lg:join-horizontal">
+      <div className="join join-vertical">
         <button className="btn justify-start join-item">
           <FaFacebook></FaFacebook>Facebook
         </button>
