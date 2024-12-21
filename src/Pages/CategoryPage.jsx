4057d856811918cb2c6d0b6f80eafbc1f2ae9889
md8-habibullah <md8.habibullah@gmail.com>
2024-12-21 19:28:54 +0600

    issuesssss cant fix

--- src/Pages/CategoryPage.jsx
+++ src/Pages/CategoryPage.jsx
@@ -1,7 +1,8 @@
-import { useLoaderData } from "react-router-dom";
+import { useLoaderData, useParams } from "react-router-dom";
 
 export default function CategoryPage() {
-  const {data} = useLoaderData()
-  console.log(data)
-  return <div>i am from page.....</div>;
+  const { data } = useLoaderData();
+  const params = useParams();
+  console.log(data);
+  return <div className="text-5xl">i am from page.....{params.id}</div>;
 }
